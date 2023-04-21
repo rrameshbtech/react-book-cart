ddeb67512592e91adf24b590a191be67dc2df1fe
Ramesh Ramalingam <rrama@thoughtworks.com>
2023-04-21 08:37:50 +0530

    add book description and image sections for details page

--- src/components/details/index.tsx
+++ src/components/details/index.tsx
@@ -30,9 +30,36 @@ function NoValidBookError() {
 }
 
 function ValidBookDetails({ book }: ValidBookDetailsProbs) {
-  return <h1>{book.title}</h1>;
+  return (
+    <div className="flex flex-col md:flex-row">
+      <div className="flex-0 w-full md:w-1/3">
+        <BookImages {...{ book }} />
+      </div>
+      <div className="flex-1">
+        <BookDescription {...{ book }} />
+      </div>
+    </div>
+  );
+}
+interface ValidBookDetailsProbs {
+  book: DetailedBook;
+}
+function BookImages({ book }: BookImagesProbs) {
+  return (
+    <img
+      className="py-4 h-48 w-full object-contain object-center"
+      src={book.thumbnailUrl}
+      alt={book.title}
+    ></img>
+  );
+}
+interface BookImagesProbs {
+  book: DetailedBook;
 }
 
-interface ValidBookDetailsProbs {
+function BookDescription({ book }: BookDescriptionProbs) {
+  return <div>{book.title}</div>;
+}
+interface BookDescriptionProbs {
   book: DetailedBook;
 }
